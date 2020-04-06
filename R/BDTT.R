8cc6e717629283178d9f5a4b1e74861a006227f5
Alexander Piper <alexpiperdesigns@gmail.com>
2020-04-06 21:29:05 +1000

    Updated BDTT to utilise speedyseq::tree_glom

--- R/BDTT.R
+++ R/BDTT.R
@@ -1,184 +1,122 @@
+# Beta diversity through time ---------------------------------------------
 
+BDTT <- function(ps, slices, metrics=c("Bray","Jaccard","Aitchison","Philr"), zeroes="Impute",  cores = 1, quiet=FALSE){
+  #Check inputs:
+  if (is.null(phy_tree(ps))) {stop("A phyloseq object with a phylogenetic tree in the phy_tree slot is required")}
+  if(!all(metrics %in% c("Bray", "Jaccard", "Aitchison", "Philr", "Unifrac", "WUnifrac"))){
+    stop("metrics must be one or more of: 'Bray', 'Jaccard', 'Aitchison', 'Philr', 'Unifrac' or 'Wunifrac'")}
 
-# New to old OTUs ---------------------------------------------------------
-
-New_toOld_OTUs_castor <- function(similarity,tree){
-  Ntips=length(tree$tip.label)
-  NameTips=tree$tip.label
+  #Define getBDTT function
+  getBDTT <- function(resolution, ps, metrics=c("Bray","Jaccard","Aitchison","Philr"), zeroes="Impute", parallel=FALSE, quiet=FALSE){
+    ps_new <- speedyseq::tree_glom(ps, resolution)
+    phy_tree(ps_new) <- multi2di(phy_tree(ps_new))
+    message(ntaxa(ps_new), " otus created from a phylogenetic slice at ", resolution)
+    if(taxa_are_rows(ps_new)){
+      otutab=t(otu_table(ps_new))
+    } else if(!taxa_are_rows(ps_new)){
+      otutab=otu_table(ps_new)
+    }
+    # If compositional stats - Deal with zeroes
+    if (any(c("Aitchison","Philr") %in% metrics) & stringr::str_to_lower(zeroes) =="pseudo"){
+      if(!quiet) {message("Adding a pseudocount for compositional distances")}
+      otutab_n0 <- prop.table(otutab + 1, margin=1)
+    } else if (any(c("Aitchison","Philr") %in% metrics) & stringr::str_to_lower(zeroes) =="impute"){
+      if(!quiet) {message("Imputing zeroes for compositional distances")}
+      otutab_n0 <- as.matrix(zCompositions::cmultRepl(otutab, method="CZM", output="prop", suppress.print=quiet))
+    }
+    ## Calculate distances
+    # Bray curtis distance
+    if ("Bray" %in% metrics){
+      Bray <- as.matrix(vegdist(otutab, method="bray"))
+    }
+    #Jaccard distance
+    if ("Jaccard" %in% metrics){
+      Jaccard <- as.matrix(vegdist(otutab, method="jac",binary = T))
+    }
+    # Aitchison distance
+    if ("Aitchison" %in% metrics){
+      Aitchison <- as.matrix(vegdist(CoDaSeq::codaSeq.clr(otutab_n0), method="euclidean")) # Replace this with custom CLR function
+    }
+    #PhilR distance 
+    if ("Philr" %in% metrics){
+      if(!quiet){
+        message("Calculating PhilR distance")
+        Philr <- as.matrix(vegdist(philr::philr(otutab_n0, phy_tree(ps_new),
+                                                part.weights='enorm.x.gm.counts',
+                                                ilr.weights='blw.sqrt'), method="euclidean"))
+        message("PhilR distance complete")
+      } else if(quiet){
+        suppressMessages(
+          Philr <- as.matrix(vegdist(philr::philr(otutab_n0, newtree,
+                                                  part.weights='enorm.x.gm.counts',
+                                                  ilr.weights='blw.sqrt'), method="euclidean"))
+        )
+      }
+    }
+    # Unweighted Unifrac distance
+    if ("Unifrac" %in% metrics){
+      if(!quiet) {message("Calculating Unweighted Unifrac distance...")}
+      Unifrac <- as.matrix(phyloseq::UniFrac(ps_new, weighted=FALSE, parallel = parallel))
+      if(!quiet) {message("Unweighted Unifrac distance complete")}
+    }
+    # Weighted Unifrac distance
+    if ("WUnifrac" %in% metrics){
+      if(!quiet) {message("Calculating Weighted Unifrac distance...")}
+      WUnifrac <- as.matrix(phyloseq::UniFrac(ps_new, weighted=TRUE, parallel = parallel))
+      if(!quiet) {message("Unweighted Unifrac distance complete")}
+    }
+    # Abind only those that are in metrics
+    AllBetas <- abind(lapply(metrics, get, envir=sys.frame(sys.parent(0))), along = 0)
+    dimnames(AllBetas)[[1]] <- metrics
+    out <- AllBetas
+    return(out)
+  }
   
-  #Collapse tips accorindg to the resolution 
-  ColapsedNodes=collapse_tree_at_resolution(tree,similarity)
-  DescendingTips=lapply(ColapsedNodes$collapsed_nodes,tree,FUN=function(i,tree){get_subtree_at_node(tree, i)$subtree$tip.label})
-  names(DescendingTips)=as.character(ColapsedNodes$collapsed_nodes+Ntips)
-  
-  #Lenght of the different categories of OTUs
-  N_newOTUS=length(DescendingTips)
-  N_collapsedOldOtus= sum(do.call(rbind, lapply(DescendingTips, function(x) length(x))))
-  N_totalNewOTUS=Ntips-N_collapsedOldOtus+N_newOTUS
-  
-  print(paste(similarity," similarity provides ",N_totalNewOTUS," total new OTUs",sep=""))
-  
-  NewOTUs_OldOTUs_Matrix=NA
-  if (N_totalNewOTUS>2) {
-    #Names of the different categories of OTUs
-    collapsedOldOtus= unlist(DescendingTips)
-    UncollapsedOTUs=NameTips[!NameTips%in%collapsedOldOtus]
+  if (cores == 1) {
+    Betas <- lapply(slices, getBDTT, ps=ps, metrics=metrics, zeroes=zeroes, parallel=FALSE, quiet=quiet)
+  } else {
+    navailcores <- parallel::detectCores()
+    if (identical(cores, "autodetect")) cores <- navailcores - 1
+    if (!(mode(cores) %in% c("numeric", "integer"))) stop("Invalid 'cores'")
+    if (cores > navailcores) stop("Number of cores is more than available")
     
-    #Creating New to Old matrix correpondance
-    NewOTUSNames=c(UncollapsedOTUs,names(DescendingTips))
-    NewOTUs_OldOTUs_Matrix=Matrix(0, ncol=N_totalNewOTUS, nrow=Ntips, dimnames = list(tree$tip.label,NewOTUSNames))
-    
-    #Filling  New to Old matrix correpondance / uncollapsed tips
-    if (length(UncollapsedOTUs)>1){diag(NewOTUs_OldOTUs_Matrix[UncollapsedOTUs,UncollapsedOTUs])=1}     #just fill the diagonal as tips are unchanged
-    if (length(UncollapsedOTUs)==1){NewOTUs_OldOTUs_Matrix[UncollapsedOTUs,UncollapsedOTUs]=1}
-    
-    #Filling  New to Old matrix correpondance / collapsed tips
-    for (i in names(DescendingTips)){NewOTUs_OldOTUs_Matrix[DescendingTips[[i]],i]=1}
-    
-  } else{print("Only 2 OTUS (or less) present at this resolution -- are you sure this is meaningfull?")} 
-  
-  newtree <- ColapsedNodes$tree
-  newtree$tip.label <- NewOTUSNames
-  out=list(CorresMatrix = NewOTUs_OldOTUs_Matrix, tree=newtree)
-  return(out)
-}
-
-getNew_OTUs_sample_matrix=function(similarity,sampleOTUs,tree){
-  New2old <- New_toOld_OTUs_castor(similarity=similarity,tree=tree) 
-  CorresMatrix <- New2old$CorresMatrix
-  newtree <- New2old$tree
-  
-  sample=Matrix(t(sampleOTUs))
-  sp=colnames(sample)
-  Newsample=t(sample[,sp] %*% CorresMatrix[sp,]) # multiplies them
-  out=list(NewSample=as.matrix(Newsample),CorrepondanceOTUs=CorresMatrix, tree=newtree)
-  return(out)
-}
-
-
-# Get Beta diversity ------------------------------------------------------
-
-getBDTT <- function(similarity, tree, sampleOTUs, metrics=c("Bray","Jaccard","Aitchison","Philr"), zeroes="Impute", parallel=FALSE, onlyBeta=TRUE, quiet=FALSE){
-  New_OTUs_sample_matrix=getNew_OTUs_sample_matrix(similarity=similarity, sampleOTUs=sampleOTUs, tree=tree)
-  OTU_table=t(New_OTUs_sample_matrix$NewSample)
-  newtree <- New_OTUs_sample_matrix$tree
-  
-  # If compositional stats - Deal with zeroes
-  if (any(c("Aitchison","Philr") %in% metrics) & stringr::str_to_lower(zeroes) =="pseudo"){
-    if(!quiet) {message("Adding a pseudocount for compositional distances")}
-    OTU_table_n0 <- prop.table(OTU_table + 1, margin=1)
-  } else if (any(c("Aitchison","Philr") %in% metrics) & stringr::str_to_lower(zeroes) =="impute"){
-    if(!quiet) {message("Imputing zeroes for compositional distances")}
-    OTU_table_n0 <- as.matrix(zCompositions::cmultRepl(OTU_table, method="CZM", output="prop", suppress.print=quiet))
-  }
-  
-  # If phylogenetic distances are used - root tree
-  if (any(c("Philr","Unifrac","WUnifrac") %in% metrics)){
-    newtree  <- multi2di(newtree)
-  }
-  ## Calculate distances
-  # Bray curtis distance
-  if ("Bray" %in% metrics){
-    Bray <- as.matrix(vegdist(OTU_table, method="bray"))
-  }
-  
-  #Jaccard distance
-  if ("Jaccard" %in% metrics){
-    Jaccard <- as.matrix(vegdist(OTU_table, method="jac",binary = T))
-  }
-  #JacTT=as.matrix(designdist(OTU_table, "(2*pmin(b,c))/(a+2*pmin(b,c))",abcd=TRUE,terms = "binary"))
-  
-  # Aitchison distance
-  if ("Aitchison" %in% metrics){
-    Aitchison <- as.matrix(vegdist(CoDaSeq::codaSeq.clr(OTU_table_n0), method="euclidean")) # Replace this with custom CLR function
-  }
-  
-  #PhilR distance 
-  if ("Philr" %in% metrics){
-    if(!quiet){
-      message("Calculating PhilR distance")
-    Philr <- as.matrix(vegdist(philr::philr(OTU_table_n0, newtree,
-                       part.weights='enorm.x.gm.counts',
-                       ilr.weights='blw.sqrt'), method="euclidean"))
-    message("PhilR distance complete")
-    } else if(quiet){
-      suppressMessages(
-        Philr <- as.matrix(vegdist(philr::philr(OTU_table_n0, newtree,
-                                              part.weights='enorm.x.gm.counts',
-                                              ilr.weights='blw.sqrt'), method="euclidean"))
-      )
+    if (cores > 1) {
+      if (!quiet) cat("Multithreading with", cores, "cores\n")
+      cores <- parallel::makeCluster(cores)
+      junk <- parallel::clusterEvalQ(cores, sapply(c("phyloseq","stringr",
+                                                     "philr", "zCompositions",
+                                                     "CoDaSeq",
+                                                     "abind", "vegan", "ape"),
+                                                   require, character.only = TRUE)) # Discard result
+      Betas <- parallel::parLapply(cores, slices, getBDTT, ps=ps, metrics=metrics, zeroes=zeroes, parallel = FALSE, quiet=quiet)
+      parallel::stopCluster(cores)
     }
   }
-  
-  # Unweighted Unifrac distance
-  if ("Unifrac" %in% metrics){
-    if(!quiet) {message("Calculating Unweighted Unifrac distance...")}
-    physeq <- phyloseq(otu_table(OTU_table, taxa_are_rows=FALSE), phy_tree(newtree))
-    Unifrac <- as.matrix(phyloseq::UniFrac(physeq, weighted=FALSE, parallel = parallel))
-    if(!quiet) {message("Unweighted Unifrac distance complete")}
-  }
-  
-  # Weighted Unifrac distance
-  if ("WUnifrac" %in% metrics){
-    if(!quiet) {message("Calculating Weighted Unifrac distance...")}
-    physeq <- phyloseq(otu_table(OTU_table, taxa_are_rows=FALSE), phy_tree(newtree))
-    WUnifrac <- as.matrix(phyloseq::UniFrac(physeq, weighted=TRUE, parallel = parallel))
-    if(!quiet) {message("Unweighted Unifrac distance complete")}
-  }
-  
-  # Abind only those that are in metrics
-  AllBetas=abind(lapply(metrics, get, envir=sys.frame(sys.parent(0))), along = 0)
-  dimnames(AllBetas)[[1]]=metrics
-  
-  if (onlyBeta==FALSE){res <- list(Beta_Div=AllBetas, 
-                            NewOTU_table=New_OTUs_sample_matrix$NewSample, 
-                            New_to_old_OTUs=New_OTUs_sample_matrix$CorrepondanceOTU,
-                            Newtree = New_OTUs_sample_matrix$tree )}
-  if (onlyBeta==TRUE){res <- AllBetas}
-  
-  return(res)
-}
-
-
-# Beta diversity function -------------------------------------------------
-
-
-BDTT <- function(similarity_slices, tree, sampleOTUs, metrics=c("Bray","Jaccard","Aitchison","Philr"), zeroes="Impute", parallel=FALSE, onlyBeta=TRUE, quiet=FALSE){
-  
-  #Check orientation of samples
-  if(!taxa_are_rows(sampleOTUs)){ 
-    sampleOTUs <- t(sampleOTUs)
-    }
-  
-  Betas <- base::lapply(similarity_slices, getBDTT, tree=tree, sampleOTUs=sampleOTUs, metrics=metrics, zeroes=zeroes, parallel=parallel, onlyBeta=onlyBeta, quiet=quiet)
-  names(Betas) <- similarity_slices
-  out <- do.call(function(...){abind(...,along=0)}, Betas)
+  names(Betas) <- slices
+  out <- do.call(function(...){abind(..., along=0)}, Betas)
   return(out)
 }
-
 
 
 # Sloans neutral model through time ---------------------------------------
 
-
-NMTT <- function(similarity_slices, tree, sampleOTUs, return="summary", quiet=FALSE){
-  #Check orientation of samples
-  if(!taxa_are_rows(sampleOTUs)){ 
-    sampleOTUs <- t(sampleOTUs)
-  }
+NMTT <- function(ps, slices, return="summary", quiet=FALSE){
+  if (is.null(phy_tree(ps))) {stop("A phyloseq object with a phylogenetic tree in the phy_tree slot is required")}
   
-  getNMTT <- function(similarity, tree, sampleOTUs, quiet=FALSE){
-    New_OTUs_sample_matrix <- getNew_OTUs_sample_matrix(similarity=similarity, sampleOTUs=sampleOTUs, tree=tree)
-    OTU_table <- t(New_OTUs_sample_matrix$NewSample)
-    newtree <- New_OTUs_sample_matrix$tree
-    out <- fit_sncm(OTU_table, pool=OTU_table)
-    #out$tree <- newtree
-    #out$otu <- OTU_table
+  getNMTT <- function(resolution, ps, quiet=FALSE){
+    ps_new <- speedyseq::tree_glom(ps, resolution)
+    message(ntaxa(ps_new), " otus created from a phylogenetic slice at ", resolution)
+    if(taxa_are_rows(ps_new)){
+      otutab=t(otu_table(ps_new))
+    } else if(!taxa_are_rows(ps_new)){
+      otutab=otu_table(ps_new)
+    }
+    out <- reltools::fit_sncm(otutab, pool=otutab)
     return(out)
   }
   
-  neutral <- lapply(similarity_slices, getNMTT, tree=tree, sampleOTUs=sampleOTUs, quiet=quiet)
-  names(neutral) <- similarity_slices
+  neutral <- lapply(slices, getNMTT, ps=ps, quiet=quiet)
+  names(neutral) <-slices
   
   # Create plots
   plotdata <- neutral %>%
